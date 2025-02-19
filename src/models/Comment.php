8c100b284a708f061fe8296d8436aa34f2393559
Yves Ponchelet <yves.ponchelet@shoku.be>
2025-02-19 17:09:46 +0100

    feat: Commentaires
    Class et Manager des commentaires

---
+++ src/models/Comment.php
@@ -0,0 +1,94 @@
+<?php
+	/* 
+		Classe Comment qui représente la table COMMENTS en base de données
+		@Author Yves P.
+		@Version 1.0
+		@Date Création: 19/02/2025
+		@Dernière modification: 19/02/2025
+	*/
+
+	class Comment 
+	{
+	    private $errors = [], $id, $nickname, $fKContent, $text, $date;
+
+	    // Constantes pour les erreurs
+		const INVALID_NICKNAME = "Le nickname n'est pas bon";
+		const INVALID_TEXT = "Le texte n'est pas conforme";
+
+	    public function __construct($values = []) {
+			if (!empty($values))
+				$this -> SettingAttributes($values);
+		}
+
+		// Méthodes
+		public function SettingAttributes($data) {
+			foreach ($data as $attribute => $value) 
+			{
+				$method = 'set'.ucfirst($attribute);
+
+				if (is_callable([$this, $method]))
+					$this -> $method($value);
+			}
+		}
+
+		public function isNew() {
+			return empty($this->id);
+		}
+
+		public function isValid() {
+			return !(empty($this->nickname) || empty($this->text));
+		}
+
+		// Setters
+		public function setId($id) {
+			$this->id = $id;
+		}
+
+		public function setNickname($nickname) {
+			if (!is_string($nickname) || empty($nickname))
+				$this->errors[] = self::INVALID_NICKNAME;
+			else
+				$this->nickname = $nickname;
+		}
+
+        public function setContent($content) {
+            $this->fKContent = $content;
+        }
+
+		public function setText($text) {
+			if (!is_string($text) || empty($text))
+				$this->errors[] = self::INVALID_TEXT;
+			else
+				$this->text = $text;
+		}
+
+        public function setDate(DateTime $datePublication) {
+            $this->date = $datePublication;
+        }
+
+		// Getters
+		public function getErrors() {
+			return $this->errors;
+		}
+
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getNickname() {
+			return $this->nickname;
+		}
+
+        public function getContent() {
+            return $this->fkContent;
+        }
+
+		public function getText() {
+			return $this->text;
+		}
+
+        public function getDate() {
+			return (new DateTime($this->date))->format("d/m/Y");
+		}
+	}	
+?>
---
+++ src/models/database/CommentManager.php
@@ -0,0 +1,62 @@
+<?php
+	/*
+		Class Manager pour les relations avec la DB sur la table COMMENTS
+		@Author Yves Ponchelet
+		@Version 1.0
+		@Creation: 19/02/2025
+		@Last update: 19/02/2025
+	*/
+
+	class CommentManager
+	{
+		private $db;
+
+		public function __construct() {
+			$this->db = (new Database())->getConnection();
+		}
+
+		// Méthodes privées
+		private function Add(Comment $comment) {
+			$query = $this->db->prepare('INSERT INTO COMMENTS (NICKNAME, FK_CONTENT, CONTENT, DTE) VALUES(:nickname, :content, :text, NOW())');
+
+			$query->bindValue(':nickname', $comment->getNickname(), PDO::PARAM_STR);
+			$query->bindValue(':content', $comment->getContent(), PDO::PARAM_INT);
+            $query->bindValue(':text', $comment->GetText(), PDO::PARAM_STR);
+
+			$query->execute();
+
+			$query->closeCursor();
+		}
+
+		// Méthodes publiques
+		public function Delete($id) {
+			$query = $this->db->prepare('DELETE FROM TAGS WHERE ID = :id');
+
+			$query->bindParam(':id', $id, PDO::PARAM_INT);
+			$query->execute();
+		}
+
+		public function Save(Comment $comment) {
+			if ($comment->isValid()) {
+				if ($comment->isNew())
+					$this->Add($comment);
+			}
+			else
+				throw new Exception($comment -> getErrors());
+		}
+
+		public function GetCommentFromContent($id) {
+			$query = 'SELECT ID id, NICKNAME nickname, CONTENT text FROM COMMENTS ORDER BY DTE';
+
+		  	$query = $this->db->prepare($query);
+			$query->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Comment');
+			$query->execute();
+
+			$listComments = $query->fetchAll();
+
+			$query->closeCursor();
+
+			return $listComments;
+		}
+	}
+?>
