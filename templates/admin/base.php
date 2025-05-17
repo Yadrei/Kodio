950f6d2bdc1bf0ac4f66b05d7f402d8f6ade123a
Yves Ponchelet <143081978+Yadrei@users.noreply.github.com>
2025-05-17 10:17:03 +0200

    feat: indexation + title
    no follow et no index + changement des libellés des title pour passer en Kodio 1.0, j'avais oublier

--- templates/admin/base.php
+++ templates/admin/base.php
@@ -3,7 +3,9 @@
 	<head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <title>Administration - Eclipse - Alpha 2.0</title>
+		<meta name="robots" content="noindex, nofollow">
+
+	    <title>Administration - Kodio 1.0</title>
 
 	    <!-- Inclusion des fichiers CSS de Bootstrap -->
 	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
--- templates/admin/login.php
+++ templates/admin/login.php
@@ -3,13 +3,14 @@
 	<head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="robots" content="noindex, nofollow">
+
+		<title>Kodio - Login administration</title>
 	    
 	    <!-- Inclusion des fichiers CSS de Bootstrap -->
 	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 	    <link type="text/css" rel="stylesheet" href="<?php echo BASE_URL ?>public/css/cms-admin-login.css" crossorigin="anonymous">
-
-	    <title>CMS - Login administration</title>
 	</head>
 	<body class="text-center">
 		    <?php
